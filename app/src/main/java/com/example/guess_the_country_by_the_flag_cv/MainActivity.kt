96c141dec9e5bf6c89909139c6aa8856cfab7737
Galii Volodymyr <progressor1985@gmail.com>
2024-01-29 20:19:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/guess_the_country_by_the_flag_cv/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guess_the_country_by_the_flag_cv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Guess-the-country-by-the-flag-CV"
+include(":app")
+ 
